f82c7afe24e7cce501513c222eac1e51ecbc0eb0
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-09-19 07:52:52 -0300

    update2
    certo

--- gameplay_OL/src/screens/signIn/index.tsx
+++ gameplay_OL/src/screens/signIn/index.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View , TextInput} from 'react-native';
 import { stylesSignIn } from './styles';
 
 
 export  function SignIn() {
   return (
     <View style={stylesSignIn.container}>
-      <Text>Essa tela é de entrada.</Text>
+      <Text>Digite algo.</Text>
+      <TextInput style = {{width :200,height:50,borderWidth:2}}/>
       <StatusBar style="auto" />
     </View>
   );
