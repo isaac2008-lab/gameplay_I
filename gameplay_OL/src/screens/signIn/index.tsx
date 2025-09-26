b1279ab9026f010f947d21626b38a97d3019bbea
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-09-26 07:42:23 -0300

    Update index.tsx
    salva por favor

--- gameplay_OL/src/screens/signIn/index.tsx
+++ gameplay_OL/src/screens/signIn/index.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View , TextInput} from 'react-native';
 import { stylesSignIn } from './styles';
-
+import {useState } from 'react' ;
 
 export  function SignIn() {
+  const[text,setText] = useState("");
   return (
     <View style={stylesSignIn.container}>
       <Text>Digite algo.</Text>
-      <TextInput style = {{width :200,height:50,borderWidth:2}}/>
+      <TextInput style = {stylesSingIn.textInput} onChangeText = {setText}/>
       <StatusBar style="auto" />
+      <Text>Você Digitou : {text}</Text>
     </View>
   );
 }
