b35b391e99671d7226eaa0c6ad36c9736f1c6016
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-10-02 07:53:54 -0300

    AAAAAA
    olko

--- gameplay_OL/src/screens/signIn/index.tsx
+++ gameplay_OL/src/screens/signIn/index.tsx
@@ -1,18 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View , TextInput} from 'react-native';
-import { stylesSignIn } from './styles';
-import {useState } from 'react' ;
+import{View,Image} from "react-native";
+
 
 export  function SignIn() {
-  const[text,setText] = useState("");
+  
   return (
-    <View style={stylesSignIn.container}>
-      <Text>Digite algo.</Text>
-      <TextInput style = {stylesSingIn.textInput} onChangeText = {setText}/>
-      <StatusBar style="auto" />
-      <Text>Você Digitou : {text}</Text>
+    <View style = {stylesSignIn.container}>
+      <Image source = {} />
     </View>
-  );
+  )
+  ;
 }
 
 
